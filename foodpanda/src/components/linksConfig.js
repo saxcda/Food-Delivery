32d29c3dddddce9519a2879223f11507234e9f90
saxcda <ericli9222@gmail.com>
2024-12-10 03:12:20 +0800

    Update Linkconfig

---
+++ foodpanda/src/components/linksConfig.js
@@ -0,0 +1,35 @@
+const linksConfig = [
+    {
+      category: "group1",
+      links: [
+        { text: "新聞室", href: "footerLink/newsroom.html" },
+        { text: "隱私權條款", href: "footerLink/隱私條款 _ foodpanda.html" },
+        { text: "下載foodpanda App", href: "footerLink/download.html" },
+        { text: "合作夥伴", href: "footerLink/partners.html" },
+        { text: "聯盟行銷合作", href: "footerLink/affiliates.html" },
+      ],
+    },
+    {
+      category: "group2",
+      links: [
+        { text: "客服中心", href: "footerLink/support.html" },
+        { text: "店家合作", href: "footerLink/vendors.html" },
+        { text: "企業徵才", href: "footerLink/careers.html" },
+        { text: "每月優惠餐廳", href: "footerLink/offers.html" },
+        { text: "退款帳戶條款與條件", href: "footerLink/refund.html" },
+      ],
+    },
+    {
+      category: "group3",
+      links: [
+        { text: "服務條款", href: "footerLink/terms.html" },
+        { text: "pandago - 媒合外送夥伴", href: "footerLink/pandago.html" },
+        { text: "foodpanda企業訂餐", href: "footerLink/corporate.html" },
+        { text: "foodpanda生鮮雜貨外送到府", href: "footerLink/groceries.html" },
+        { text: "所有城市", href: "footerLink/cities.html" },
+      ],
+    },
+  ];
+  
+  export default linksConfig;
+  
